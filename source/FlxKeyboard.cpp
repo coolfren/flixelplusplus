1ef241b05ccf1e211ee0adf63ecf1e51d00b93e8
coolfren <trollengine@protonmail.com>
2022-12-31 01:24:13 +0100

    cmake curl

--- source/FlxKeyboard.cpp
+++ source/FlxKeyboard.cpp
@@ -1,17 +1,20 @@
 #include "flixel++/FlxKeyboard.hpp"
+#include <cstring>
 
 Flx::Keyboard::Keyboard()
     : keys(new bool[255])
 {
+    memset(keys, false, 255);
 }
 
-bool Flx::Keyboard::pressed(const unsigned  char key)
+bool Flx::Keyboard::pressed(const unsigned char key)
 {
     return keys[key];
 }
 
 bool Flx::Keyboard::justPressed(const unsigned char key){
     bool pressed = keys[key];
-    keys[key] = false;
+    if(pressed)
+        keys[key] = false;
     return pressed;
 }
