fc6c8b5c5672934fa3f7584d41b20b1c4cffe08c
GDev28 <gcgranja@gmail.com>
2022-12-31 00:23:56 +0000

    FlxHttp fixes and saul goodman

--- examples/State.hpp
+++ examples/State.hpp
@@ -6,6 +6,7 @@
 #include "flixel++/FlxG.hpp"
 #include "flixel++/FlxText.hpp"
 #include "flixel++/FlxMacros.hpp"
+#include "flixel++/FlxHttp.hpp"
 
 class PlayState : public Flx::State
 {
@@ -13,27 +14,46 @@ class PlayState : public Flx::State
     PlayState(){};
     ~PlayState(){};
     Flx::Sprite* sprite;
+    Flx::Text* text;
+    Flx::Http* http;
     float time = 0.0f;
     float offsetX = 0.0f, offsetY = 0.0f;
     void create(){
+        http = new Flx::Http("https://pastebin.com/raw/j9cs4GWP");
+        trace(http->storage);
+
         sprite = new Flx::Sprite(0,0);
-        sprite->loadGraphic("assets/logo.png");
-        sprite->setGraphicSize(240,240);
+        if(http->storage == "saul goodman"){
+            sprite->loadGraphic("assets/Saul.png");
+            sprite->setGraphicSize(800,800);
+        }
+        else{
+            sprite->loadGraphic("assets/logo.png");
+            sprite->setGraphicSize(240,240);
+        }
+
         sprite->screenCenter();
         add(sprite);
 
-        Flx::Text* text = new Flx::Text(0,100, "i am a piece of text!");
-        text->screenCenter();
-        text->setText("i am a piece of new text!");
+        text = new Flx::Text(0,100, "i am a piece of text!");
+        if(http->storage == "saul goodman"){
+            text->screenCenter();
+            text->setText(http->storage + " V1.0");
+        }
+        else{
+            text->x = (Flx::Globals::width / 2);
+            text->setText("i am a piece of new text!");
+        }
         add(text);
 
     };
     virtual void update(){
-        sprite->x += cos(time) + offsetX;
-        sprite->y += sin(time) + offsetY;
-        
+        sprite->x += cos(time) + offsetX * 2;
+        sprite->y += sin(time) + offsetY * 2;
+        text->x -= cos(time) - offsetX * 2;
+        text->y -= sin(time) - offsetY * 2;
 
-        time += 0.05f;
+          time += 0.05f;
         Flx::State::update();
     }
 };
