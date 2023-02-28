a7b5add13f3b375b416d37b0438f6100eb28bbab
coolfren <90194420+coolfren@users.noreply.github.com>
2023-02-28 11:45:12 +0100

    fixing shaders

--- include/flixel++/FlxShader.hpp
+++ include/flixel++/FlxShader.hpp
@@ -4,10 +4,10 @@
 #define _FLIXEL_DEFAULTVSHADER "#version 330 core\n" \
 "layout (location = 0) in vec3 position;\n" \
 "void main(){\n" \
-"glPosition = vec4(position.x, position.y, position.z, 1.0);\n" \
+"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n" \
 "}\n"
 
-#define _FLIXEL_DEFAULTFSHADER "version 330 core\n" \
+#define _FLIXEL_DEFAULTFSHADER "#version 330 core\n" \
 "out vec4 FragColor;\n" \
 "void main(){\n" \
 "FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n" \
