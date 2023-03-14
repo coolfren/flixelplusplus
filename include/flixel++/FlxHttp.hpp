ec9c3a8feceaf9470a63f76813da608f5ae4a0ff
GDev28 <gcgranja@gmail.com>
2023-03-14 19:11:44 +0000

    Fixes?

---
+++ include/flixel++/FlxHttp.hpp
@@ -0,0 +1,33 @@
+#ifndef FLIXEL_HTTP
+#define FLIXEL_HTTP
+
+#include "flixel++/Common.hpp"
+#ifdef FLIXEL_USE_CURL
+#include "curl/curl.h"
+
+//NEED TO REWORK ON THIS
+
+namespace Flx
+{
+    class Http
+    {
+    public:
+        /// @brief Creates an new Http point
+        Http();
+        
+        ~Http();
+
+        CURL *curl;
+
+        std::string requestURLtext(const std::string&);
+
+        std::string storage;
+        bool onData;
+        bool onError;
+        bool received;
+
+        static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp);
+    };
+}
+#endif
+#endif
---
+++ include/flixel++/FlxObject.hpp
@@ -0,0 +1,29 @@
+#ifndef FLXOBJECT_HPP
+#define FLXOBJECT_HPP
+#include "flixel++/FlxBasic.hpp"
+
+namespace Flx
+{
+    class Object : public Flx::Basic
+    {
+        public:
+        
+        /// @brief X position of an object normally located at the upper left corner of the object
+        float x = 0;
+        /// @brief Y position of an object normally located at the upper left corner of the object
+        float y = 0;
+
+        /// @brief Sets the width of an object (Not of an texture)
+        float width = 0;
+        /// @brief Sets the height of an object (Not of an texture)
+        float height = 0;
+        
+        /// @brief Base of the engine other resources demonstrated as an invisible object
+        /// @param x X position of the object
+        /// @param y Y position of the object
+        Object(float x, float y);
+        
+        ~Object();
+    };
+}
+#endif
